8ac4e91e0a6ea9503b4a686fcaa92dc36014e746
Jenny Knuth <inkboco@gmail.com>
2015-06-04 09:06:25 -0600

    grade is working with + and -, time to add more functions

--- grader.js
+++ grader.js
@@ -1,1 +1,46 @@
-module.exports = ('hi there!');
+module.exports = {
+
+  grade: function(num){
+    var grade = '';
+    var firstDigit = parseInt((num + '').charAt(0));
+    var secondDigit = parseInt((num + '').charAt(1));
+
+    if (num === 100){
+      return grade = 'A+';
+    }
+    else if (firstDigit<=5){
+      return grade = 'F';
+    }
+    else if (firstDigit <= 6){
+      grade = 'D';
+    }
+    else if (firstDigit <= 7){
+      grade = 'C';
+    }
+    else if (firstDigit <= 8){
+      grade = 'B';
+    }
+    else{
+      grade = 'A';
+    }
+
+    //add + or -
+    if (secondDigit < 4){
+      grade = grade + '-';
+    }
+    else if (secondDigit > 6 && secondDigit <= 9){
+      grade = grade + '+';
+    }
+    return grade;
+  },
+
+  average: function(arr){
+
+
+  }
+
+
+
+
+
+}
--- spec/spec.js
+++ spec/spec.js
@@ -2,10 +2,15 @@ var grader = require('./../grader.js');
 
 describe('#grade()', function(){
  it('takes a test score and returns the equivalent letter grade.', function() {
-   expect(grader.grade(98)).toEqual('A');
-   expect(myCode.sum(13, 14)).toEqual(27);
+   expect(grader.grade(98)).toEqual('A+');
+   expect(grader.grade(100)).toEqual('A+');
+   expect(grader.grade(89)).toEqual('B+');
+   expect(grader.grade(75)).toEqual('C');
+   expect(grader.grade(72)).toEqual('C-');
+   expect(grader.grade(59)).toEqual('F');
+   expect(grader.grade(0)).toEqual('F');
  });
- it('take two strings and concatenate', function() {
+ xit('take two strings and concatenate', function() {
    expect(myCode.sum('foo', 'bar')).toEqual('foobar');
  });
 });
